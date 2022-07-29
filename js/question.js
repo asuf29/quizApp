5016f416dba1ec7999628bb77cbb879d230c25fb
asuf29 <asuf29@icloud.com>
2022-07-29 15:23:01 +0300

    question.js modified

--- js/question.js
+++ js/question.js
@@ -9,8 +9,8 @@ Question.prototype.checkTheAnswer = function (answer) {
 }
 
 let questions = [
-    new Question("1-aaaaascsx", {a:"asd", b:"gvhbjn", c:"asxd"}, "a"),
-    new Question("2-aaaaascsx", {a:"asd", b:"gvhbjn", c:"asxd"}, "b"),
-    new Question("3-aaaaascsx", {a:"asd", b:"gvhbjn", c:"asxd"}, "c"),
-    new Question("4-aaaaascsx", {a:"asd", b:"gvhbjn", c:"asxd"}, "a"),
+    new Question("1-Ana belleğin sadece okunabilen, silinemeyen ve değiştirilemeyen kısmına ne ad verilir?", {a:"RAM Bellek", b:"ROM Bellek", c:"Hard Disk", d:"CPU"}, "b"),
+    new Question("2-Aşağıdakilerden hangisi bir programlama dili değildir?", {a:"Paskal", b:"Cobol", c:"Windows XP", d:"Delphi"}, "c"),
+    new Question("3-Aşağıdakilerden hangisi bir yazılım çeşidi değildir?", {a:"İşletim sistemleri", b:"Uygulama yazılımları", c:"Bilgi otobanı", d:"Programlama dilleri"}, "c"),
+    new Question("4-Tarayıcının işlevi nedir?", {a:"Bilgisayardaki bilgileri kâğıda aktarmak", b:"Kâğıttaki bilgileri bilgisayara aktarmak", c:"İnternette aradığımız bir konuyu taramak", d:"Bilgisayardaki dosyaları taramak"}, "b"),
 ];
